43e74c43c1964c9581445cd86cad3b7209a58ca0
Khin Thandar Tun <khinthandartun738@gmail.com>
2025-08-03 22:30:26 +0630

    Create TodoLists.vue

---
+++ src/components/TodoLists.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <h3 class="text-primary text-center">All Todos</h3>
+    <div class="container">
+      <div class="row">
+        <AddTodo />
+      </div>
+      <div class="row">
+        <div class="col-md-4 my-4" v-for="todo in myTodos" :key="todo.id">
+           <b-card
+        border-variant="primary"
+        text-variant="primary"
+        class="text-center"
+        align="center"
+      >
+        <b-card-text>{{ todo.title }}</b-card-text>
+      </b-card>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapActions, mapGetters } from 'vuex';
+import AddTodo from './AddTodo.vue';
+
+export default {
+  components: { AddTodo },
+  computed: {
+  ... mapGetters([
+    "myTodos"
+  ])
+  },
+  methods:mapActions(['getTodos']),
+  mounted(){
+   this.getTodos();
+  }
+};
+</script>
+
+<style>
+
+</style>
