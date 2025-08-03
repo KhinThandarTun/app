c93d3463d2e1fc3f4c4e65556370cae72ef0a20a
Khin Thandar Tun <khinthandartun738@gmail.com>
2025-08-03 23:03:38 +0630

    Update Todos.js

--- src/store/modules/Todos.js
+++ src/store/modules/Todos.js
@@ -8,7 +8,15 @@ export default {
     mutations: {
         setTodos(state, todos){
             state.todos = todos;
-        }
+        },
+        setTodo(state, newTodo){
+            state.todos.unshift(newTodo);
+        },
+        removeTodo(state, removeId){
+            state.todos = state.todos.filter(t => {
+                return t.id != removeId
+            })
+        },
     },
     getters: {
         myTodos(state){
@@ -21,5 +29,17 @@ export default {
             let todos = response.data;
             commit('setTodos', todos);
         },
+        async addTodo({commit}, newTodo)
+        {
+             let res = await
+              axios.post('https://jsonplaceholder.typicode.com/todos',
+                 newTodo
+                );
+                commit('setTodo', res.data);
+        },
+        async deleteTodo(context, removeId){
+           await axios.delete('https://jsonplaceholder.typicode.com/todos/${removeId}')
+            context.commit('removeTodo', removeId);
+        }
     },
 };
