bae9fa5d88ff6d2c734a45f266e906ecc57c9c60
Khin Thandar Tun <khinthandartun738@gmail.com>
2025-08-03 22:30:24 +0630

    Create AddTodo.vue

---
+++ src/components/AddTodo.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div>
+  <b-input-group
+    v-for="size in ['sm','','lg']"
+    :key="size"
+    :size="size"
+    class="mb-3"
+    prepend="Label"
+  >
+    <b-form-input></b-form-input>
+    <b-input-group-append>
+      <b-button size="sm" text="Button" variant="success">Button</b-button>
+    </b-input-group-append>
+  </b-input-group>
+</div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
